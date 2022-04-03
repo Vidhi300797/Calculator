d231dd577962428796ac9c4a79e945164b447a8e
Rit45sin <riteshsingh@supporthives.in>
2022-04-04 03:20:01 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,140 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+
+import backicon from './back_icon.png';
+
+const keypadArr = [
+    ["AC", "back", "%", "/"],
+    ["7", "8", "9", "X"],
+    ["4", "5", "6", "-"],
+    ["1", "2", "3", "+"],
+    ["0", ".", "="]
+]
+
+const operationArr = ["+", "-", "/", "*", "X", "x", "%", 56];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [inputText, setInputText] = useState("");
+    const [calc, setCalc] = useState("");
+    const [result, setResult] = useState("empty");
+
+    useEffect(() => {
+        if(result == Infinity) {
+            console.log("here");
+        }
+        try{
+        if (inputText != ".") {
+            let express = inputText;
+            express = express.replace("x", "*")
+            console.log("exp", express);
+            if (!operationArr.includes(inputText[inputText.length - 1])) {
+                console.log("eval", eval(express), express);
+                setResult(eval(express));
+            }
+            if (!inputText) {
+                setResult("empty")
+            }
+        }
+    }
+    catch(err) {
+        alert("error", err);
+    }
+    }, [inputText])
+
+
+    const handleTextChange = (val) => {
+        setInputText(val);
+    }
+
+    const handleKeyPress = (e) => {
+        console.log(e, inputText)
+        if (e.keyCode == 56 && e.key == "*") {
+            if (operationArr.includes(inputText[inputText.length - 1])) {
+                let val = inputText;
+                val = val.slice(0, val.length - 1);
+                setInputText(val + e.key);
+            }
+            else {
+                setInputText(inputText + e.key);
+            }
+        }
+        else if (e.keyCode >= 48 && e.keyCode <= 57 || e.keyCode == 190) {
+            setInputText(inputText + e.key);
+        }
+        else if (e.keyCode == 8) {
+            let val = inputText;
+            val = val.slice(0, val.length - 1);
+            setInputText(val);
+        }
+        else if (operationArr.includes(e.key) || operationArr.includes(e.keyCode)) {
+            if (operationArr.includes(inputText[inputText.length - 1])) {
+                let val = inputText;
+                val = val.slice(0, val.length - 1);
+                setInputText(val + e.key);
+            }
+            else {
+                setInputText(inputText + e.key);
+            }
+
+        }
+        else if (e.keyCode == 13) {
+            if(result != "empty" || result != Infinity || result != "Infinity") setInputText(`${result}`);
+        }
+
+
+    }
+
+    const handleOnScreenKey = (key) => {
+        if (key == "AC") {
+            setInputText("");
+            setResult("empty");
+        }
+        else if (key == "=") {
+            setInputText(`${result}`);
+        }
+        else if(key == "back") {
+            let val = inputText;
+            val = val.slice(0, val.length - 1);
+            setInputText(val);
+        }
+        else {
+            if (operationArr.includes(key)) {
+                if (operationArr.includes(inputText[inputText.length - 1])) {
+                    let val = inputText;
+                    val = val.slice(0, val.length - 1);
+                    setInputText(key);
+                }
+                else {
+                    setInputText(inputText + key);
+                }
+            }
+            else {
+                setInputText(inputText + key);
+            }
+        }
+    }
+
+    return (
+        <div className="main-container">
+            <div className="sub-container">
+                <div className="text-box">
+                    <input className="input-exp" autoFocus type="text"  value={inputText} onKeyUp={handleKeyPress} />
+                </div>
+                <div className="result">{result == "empty" ? "0" : "=" + result}</div>
+                <div className="keypad">
+                    {keypadArr.map((row) =>
+                        <div className="keypad-row">
+                            {row.map((sign, i) =>
+                                sign && <button key={i} onClick={() => handleOnScreenKey(sign)} style={{ width: row.length == 4 ? "20%" : row.length == 3 && i == 2 ? "46.6%" : "20%" }}>{sign == "back" ? <img src={backicon} width="20px" alt="back"/> : sign || ""}</button>
+                            )}
+                        </div>)
+                    }
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
